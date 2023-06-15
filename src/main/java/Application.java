1e93e3fceb89e8f4849e7c9cfb9622cea221fd14
Spolpinka <a421243266@gmail.com>
2023-06-15 14:54:01 +0300

    Реализованы CRUD через Hibernate

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Application {
+import dao.EmployeeDAO;
+import dao.impls.EmployeeDAOImpl;
+import model.Employee;
+
+public class Application {
+    public static void main(String[] args) {
+        EmployeeDAO employeeDAO = new EmployeeDAOImpl();
+        //получение всего списка сотрудников
+        employeeDAO.getAllEmployee().forEach(System.out::println);
+
+        //создаем нового сотрудника
+        Employee employee = new Employee("Gadya", "Petrovich-Hrenovo", "female", 35, 3);
+        //добавляем его в таблицу employee
+        employeeDAO.save(employee);
+
+        //получаем единичного сотрудника по id
+        Employee employeeForUpdate = employeeDAO.getEmployeeById(1);
+        System.out.println(employeeForUpdate);
+
+        //Изменение сотрудника
+        employeeForUpdate.setFirstName("Gadya Petrovich Hrenovo");
+        employeeDAO.update(employeeForUpdate);
+
+
+        //удаляем сотрудника
+        employeeDAO.delete(employee);
+
+    }
 }
--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -1,2 +1,17 @@
-package dao;public interface EmployeeDAO {
+package dao;
+
+import model.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> getAllEmployee();
+
+    void save(Employee employee);
+
+    Employee getEmployeeById(int id);
+
+    void update(Employee employee);
+
+    void delete(Employee employee);
 }
